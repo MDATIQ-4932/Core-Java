a848e67ec60f181318fcf8bf0e7fabd04e7a6719
MDATIQ-4932 <mdatiqulislam59@gmail.com>
2025-03-24 16:21:04 +0600

    ADD
    use scanner to add integer number

---
+++ DataTypePractise/src/datatypepractise/DataTypePractise.java
@@ -0,0 +1,18 @@
+
+package datatypepractise;
+
+
+public class DataTypePractise {
+
+    public static void main(String[] args) {
+     byte id =120;
+     boolean status =true;
+     char letter = '\u0041';
+     String name ="atiq";
+     float number =111122.555f;
+     double doublenumber =25552000110.554;
+     
+     System.out.println(letter);
+    }
+    
+}
