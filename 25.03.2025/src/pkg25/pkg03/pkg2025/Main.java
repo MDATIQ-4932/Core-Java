35cebbf1154722324f8ad43304a6588e5a00dfc4
MDATIQ-4932 <mdatiqulislam59@gmail.com>
2025-03-25 15:55:30 +0600

    add

---
+++ 25.03.2025/src/pkg25/pkg03/pkg2025/Main.java
@@ -0,0 +1,27 @@
+
+package pkg25.pkg03.pkg2025;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+     
+        Scanner input =new Scanner(System.in);
+        
+   System.out.println("seconds");
+  int totalSeconds =input.nextInt();
+ int hours = totalSeconds / 3600;
+ int remainingSeconds =totalSeconds %3600;
+ int minutes =remainingSeconds /60;
+ int seconds =remainingSeconds /60; 
+  
+  
+          
+         
+    }
+   
+    
+}
