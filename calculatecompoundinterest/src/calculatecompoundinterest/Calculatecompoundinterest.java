5ecb42fb8518c2397c5776e2f21fff85e335cd4d
MDATIQ-4932 <mdatiqulislam59@gmail.com>
2025-04-12 16:26:48 +0600

    ADD
    interest& factorial value

---
+++ calculatecompoundinterest/src/calculatecompoundinterest/Calculatecompoundinterest.java
@@ -0,0 +1,31 @@
+package calculatecompoundinterest;
+
+import java.util.Scanner;
+
+public class Calculatecompoundinterest {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter Interest rate Like as 12.5% ");
+        double interestRate = s.nextDouble();
+
+        double monthlyInterestRate = interestRate / 1200;
+
+        System.out.println("Enter Nnumber of Year ");
+        int numberOfYears = s.nextInt();
+
+        System.out.println("Enter Loan amount ");
+        double loanAmount = s.nextDouble();
+
+        double monthlyPayment = loanAmount * monthlyInterestRate / (1
+                - 1 / Math.pow(1 + monthlyInterestRate, numberOfYears * 12));
+
+        double totalPayment = monthlyPayment * numberOfYears * 12;
+
+        System.out.println("The monthly payment is BDT " + (int) monthlyPayment);
+        System.out.println("The total payment is BDT " + (int) totalPayment);
+    }
+
+}
