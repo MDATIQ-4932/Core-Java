8b1b6d2e90c0dd9da50f89c475ba2552b71038e3
MDATIQ-4932 <mdatiqulislam59@gmail.com>
2025-04-13 17:20:26 +0600

    ADD
    Using array

---
+++ Arrays/src/arrays/usearrays.java
@@ -0,0 +1,20 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class usearrays {
+
+    public static void main(String[] args) {
+
+        String[] names = {"atiq", "ieran", "shemantho", "masud", "reja"};
+        int[] marks = {50, 60, 70, 80, 90};
+
+        for( int index=0; index<names.length; index++){
+        
+            System.out.println("my name is " + names[index]+" and markes is " + marks[index]);   
+            
+        }
+        
+    }
+
+}
