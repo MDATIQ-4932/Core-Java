d617dc04b8dcc802136235c8d521d21ac9efa740
MDATIQ-4932 <mdatiqulislam59@gmail.com>
2025-05-07 17:52:08 +0600

    ADD

---
+++ JAVA MID-TERM/InsertionSort/src/insertionsort/InsertionSort.java
@@ -0,0 +1,38 @@
+
+package insertionsort;
+
+import java.util.Arrays;
+
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+     
+     int[] numbers= {50,10,20,5,8,50,70,5,100,90};
+        
+        insertionSort(numbers);
+        
+    }
+    
+    public static void insertionSort(int[] list) {
+        
+        for (int i = 1; i < list.length; i++) {
+           
+            int currentElement = list[i];
+            int k;
+            // {50,10,20,5,8}
+            for (k = i - 1; k >= 0 && list[k] > currentElement; k--) {
+                list[k + 1] = list[k];
+                
+            }
+            
+            list[k + 1] = currentElement;
+        }
+        
+        System.out.println(Arrays.toString(list));   
+        
+        
+        
+    }
+    
+}
