14d74bc4a9bad7c2191e0603d829a639e39d020b
MDATIQ-4932 <mdatiqulislam59@gmail.com>
2025-05-10 16:23:22 +0600

    Changed

---
+++ ThreadLearn/src/threadlearn/ThreadLearn.java
@@ -0,0 +1,27 @@
+
+package threadlearn;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+public class ThreadLearn {
+
+    
+    public static void main(String[] args) {
+        
+        ExecutorService executor = Executors.newFixedThreadPool(4);   
+        
+       
+          for (int i = 1; i <= 10; i++) {
+            Runnable task = new ThreadLearnClass(i);
+            executor.submit(task);
+        }
+
+       
+        executor.shutdown();
+    }
+    
+}
+
+    
---
+++ ThreadLearn/src/threadlearn/ThreadLearnClass.java
@@ -0,0 +1,28 @@
+
+package threadlearn;
+
+
+public class ThreadLearnClass implements Runnable {
+   
+     private int taskId;
+
+    public ThreadLearnClass(int taskId) {
+        this.taskId = taskId;
+    }
+
+    @Override
+    public void run() {
+       System.out.println("Task " + taskId + " is running on thread " + Thread.currentThread().getName());
+       
+        try {
+             Thread.sleep(1000);
+            
+            
+        }
+        catch (InterruptedException e) {
+            
+             System.out.println("Task " + taskId + " was interrupted");
+        }
+        System.out.println("Task " + taskId + " completed by " + Thread.currentThread().getName());
+    }
+}
